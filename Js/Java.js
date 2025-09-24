075bcae531629e3f1a93c670e5e7220cb2203d7b
sama manavi <samamanavi1881@gmail.com>
2025-09-24 20:43:34 +0330

    initial project

---
+++ Js/Java.js
@@ -0,0 +1,5 @@
+let date = new Date().toLocaleDateString('fa-IR');
+document.getElementById('date').innerHTML= date;
+
+let dateMobile = new Date().toLocaleDateString('fa-IR');
+document.getElementById('date-mobile').innerHTML= dateMobile;
